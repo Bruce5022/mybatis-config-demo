63963138be59aa682b82fbddcc8cde9e222cd6cb
szw <shizhanwei123@aliyun.com>
2020-01-14 23:07:19 +0800

    mybatis全局配置文件 szw

---
+++ src/main/java/com/sky/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.sky.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
